56aeee93c38854eaa4d0d2b2eb49993f79dfecb1
mickagamer_ <32200544+mickagamer@users.noreply.github.com>
2018-07-02 06:35:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NDYzMTgzMzU2MTE3NjQ3MzYw.DhstmQ.7G0aDKJxGy33UjvT8nx78ODBbCo';
+
+bot.login(process.env.TOKEN);
 
 var prefix = ("/")
 
